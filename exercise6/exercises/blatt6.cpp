d0ae09b529907af448b65d984f785ac8d536678c
atlas <lukas.s.fuchs@gmx.de>
2025-06-12 12:57:15 +0200

    exercise 5 unfineshed and exercise6_sheet

---
+++ exercise6/exercises/blatt6.cpp
@@ -0,0 +1,37 @@
+#include "../include/blatt6.h"
+
+// exercise 2
+// constant is setting the maximum values of n and k
+const int N = 1000;
+
+long long binom_memo(int n, int k) {
+    // TODO
+    return 0;
+}
+
+//exercise 3
+void selection_sort(vector<int>& vec) {
+    // TODO
+}
+
+//exercise 4a
+int search(vector<int>& vec, int c, int x) {
+    // TODO
+    return 0;
+}
+
+//exercise 4b
+int get_shift(vector<int>& vec) {
+    // TODO
+    return 0;
+}
+
+
+
+/**
+ *  ========== Helperfunction ==========
+ *
+ *  void printList(vector<int>& numbers)
+ *  -> Prints the List
+ *
+ */
